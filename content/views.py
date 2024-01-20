940296eac9150b61cc666cd6f9d51f28da439a20
jillusc <>
2024-01-20 21:02:13 +0000

    Created ReviewsList view

--- content/views.py
+++ content/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Review, Film
 
 # Create your views here.
+
+
+class ReviewsList(generic.ListView):
+    queryset = Review.objects.all()
+    template_name = "content/reviews_list.html"
