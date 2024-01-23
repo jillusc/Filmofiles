e59951ab0d552bbf394cdac844cbcce5b1e0af57
jillusc <>
2024-01-23 08:38:20 +0000

    Set up css

--- content/views.py
+++ content/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Review, Film
-
-# Create your views here.
+from .models import Review, Comment, Film
 
 
 class ReviewsList(generic.ListView):
