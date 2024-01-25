cccce74dcf430fc0a255d06f7ae15c98e445288d
jillusc <>
2024-01-25 13:22:02 +0000

    Added necessary imports in views.py related to last commit, ie.  form submission

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.models import User
 from .forms import SignUpForm, LogInForm
 
 
